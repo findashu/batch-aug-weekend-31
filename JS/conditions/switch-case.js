735359a940516363ebfb78ded0f3f3043d5a9412
Ashu_Mishra <ashutosh@thruskills.com>
2019-09-21 19:07:19 +0530

    css and js loops & conditional statement

---
+++ JS/conditions/switch-case.js
@@ -0,0 +1,13 @@
+var lang = "Java";
+
+
+switch (lang) {
+    case 'JS' :
+        console.log('Lang is JS');
+    break;
+    case 'Python' : 
+        console.log('Lang is Python');
+    break;
+    default : 
+        console.log('Choose a lang')
+}
