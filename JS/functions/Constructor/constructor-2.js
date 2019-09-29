1c77baffacb1907ec2786db5a34a053a265b3b7d
Ashu_Mishra <ashutosh@thruskills.com>
2019-09-29 18:59:54 +0530

    css selector js constructor fn

---
+++ JS/functions/Constructor/constructor-2.js
@@ -0,0 +1,50 @@
+function Gadget(name,color) {
+    this.name = name;
+    this.color = color;
+}
+
+Gadget.price = 20;
+
+Gadget.prototype.company = 'Google';
+
+
+console.log(Gadget.price);
+
+var watch = new Gadget('titan','brown');
+
+var marker = new watch.constructor('camlin','blue')
+
+console.log(watch.constructor)
+
+console.log(watch.name);
+
+console.log(watch.price);
+
+console.log('trying to access prototype ',watch.company)
+
+console.log(watch.hasOwnProperty('company'));
+
+console.log(watch.constructor.prototype.hasOwnProperty('company'));
+console.log('trying to find toString')
+
+console.log(watch.hasOwnProperty('toString'));
+console.log(watch.constructor.hasOwnProperty('toString'));
+console.log(watch.constructor.prototype.hasOwnProperty('toString'));
+
+
+console.log(Object.hasOwnProperty('toString'));
+console.log(Object.constructor.prototype.hasOwnProperty('toString'));
+
+
+
+
+
+
+let n = watch.toString(10);
+
+console.log(n);
+
+
+
+
+console.log(marker);
--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 * NodeJS
 * ExpressJS
 * MongoDB (native driver + mongoose orm)
+
