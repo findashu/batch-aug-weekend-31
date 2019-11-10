06b25f0e73fecd628a05231490921be35ba7b27a
Ashu_Mishra <ashutosh@thruskills.com>
2019-11-10 19:17:47 +0530

    todo app and promise

--- JS/Browser/Exercise/todo-app/app.js
+++ JS/Browser/Exercise/todo-app/app.js
@@ -10,37 +10,50 @@ function addTodoItem() {
         value: value,
         checked: false
     }
-
-
     items.push(data);
-
     localStorage.setItem('list', JSON.stringify(items))
-
     renderList(items);
+    inpt.value = '';
+    inpt.focus();
 }
-
 
 function renderList(list) {
    displayList.innerHTML =  list.map((ele,i) => {
         return `
             <li>
-                <input type="checkbox" id="inpt${i}" ${ele.checked ? 'checked': ''} />
+                <input type="checkbox" data-id=${i} id="inpt${i}" ${ele.checked ? 'checked': ''} />
                 <label>${ele.value}</label>
             </li>
         `
     }).join('')
 }
 
-
-let checkbx = document.querySelector('inpt');
-
-
 renderList(items)
 
+function toggle(e) {
+    // console.log(this)
+    // console.log(e.target);
 
-displayList.addEventListener('click', function(e) {
-    console.log(this)
-    console.log(e.target);
-})
+    if(!e.target.matches('input')) return;
+
+    let ele = e.target;
+
+    let index = parseInt(ele.dataset.id);
+
+    // if(items[index].checked) {
+    //     items[index].checked = false
+    // }else {
+    //     items[index].checked = true
+    // }
+
+    items[index].checked = !items[index].checked
+
+    localStorage.setItem('list', JSON.stringify(items))
+
+    renderList(items)
+
+} 
+
+displayList.addEventListener('click', toggle)
 
 btn.addEventListener('click', addTodoItem)
